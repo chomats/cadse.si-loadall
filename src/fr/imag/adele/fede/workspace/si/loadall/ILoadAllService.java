ed25d25da2c0629847e5f218703e5c1c6faf14df
chomats <chomats@kasparov.imag.fr>
2009-09-18 07:17:00 +0000

    Partager projet "fr.imag.adele.cadse.si.workspace.loadall" dans "http://henry.imag.fr/svn/fede-repos/branches/r_2_2_0"

---
+++ src/fr/imag/adele/fede/workspace/si/loadall/ILoadAllService.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package fr.imag.adele.fede.workspace.si.loadall;
+
+import fr.imag.adele.cadse.core.CadseDomain;
+import fr.imag.adele.cadse.core.CadseRuntime;
+import fr.imag.adele.fede.workspace.as.initmodel.IInitModel;
+import fr.imag.adele.fede.workspace.as.persistence.IPersistence;
+import fr.imag.adele.fede.workspace.as.platformeclipse.IPlatformEclipse;
+
+public interface ILoadAllService {
+
+	public abstract IPersistence getPersistenceService();
+
+	public abstract IInitModel getInitModelService();
+
+	public abstract CadseDomain getCadseDomain();
+
+	public abstract IPlatformEclipse getPlatformEclipseService();
+
+	public abstract CadseRuntime[] openDialog(boolean askToErase);
+
+}
